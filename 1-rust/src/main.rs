f54b7402824f9557c5ce05832cc4f4295839f8ad
t1end4t <tiendatarse3004@gmail.com>
2025-06-19 10:11:19 +0700

    haskell after python

--- 1-rust/src/main.rs
+++
@@ -1,9 +0,0 @@
-use advent_of_code::sorts::bead_sort;
-
-fn main() {
-    let arr = vec![6, 11, 12, 4, 1, 5];
-    let result = bead_sort(arr);
-
-    // print out result
-    println!("Sorted array: {:?}", result);
-}
